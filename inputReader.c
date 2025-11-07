83b0fe4f9928a5a0d74bbfb56be5554a75914a76
Jacob Todd <jacobt0722@gmail.com>
2025-11-07 10:03:14 -0800

    got the frames decoded and scaled. I now need to reencode the new downscaled video and put it in its own avContext to write it a file.

--- inputReader.c
+++ inputReader.c
@@ -30,14 +30,108 @@ int parseVideoFile(const char* fileName, AVHandle *fmtHandle){
     }
     if(fmtHandle->audioIndex == -1){
         fprintf(stderr, "Failed to find audio stream\n");
-        return 4;
+        return 5;
     }
 
     return 0;
 }
 
+
+AVFrame *createFrame(AVHandle *fmtHandle, int downScaleFactor){
+    AVFrame *frame = av_frame_alloc();
+    if(frame == NULL){
+        fprintf(stderr, "frame failed to init\n");
+        return NULL;
+    }
+
+    frame->width = fmtHandle->fmtCtx->streams[fmtHandle->videoIndex]->codecpar->width / downScaleFactor;
+    frame->height = fmtHandle->fmtCtx->streams[fmtHandle->videoIndex]->codecpar->height / downScaleFactor;
+    frame->format = fmtHandle->fmtCtx->streams[fmtHandle->videoIndex]->codecpar->format;
+    if(av_frame_get_buffer(frame, 0) != 0){
+        fprintf(stderr, "frame buffer failed to create\n");
+        return NULL;
+    }
+
+    return frame;
+}
+
+
+AVCodecContext *createCodecContext(AVHandle *fmtHandle){
+    const AVCodec *decoder = avcodec_find_decoder(fmtHandle->fmtCtx->streams[fmtHandle->videoIndex]->codecpar->codec_id);
+    if(decoder == NULL){
+        fprintf(stderr, "Decoder failed to init\n");
+        return NULL;
+    }
+    AVCodecContext *avCtx = avcodec_alloc_context3(decoder);
+    if(avCtx == NULL){
+        fprintf(stderr, "Failed to alloc AVCodecContext\n");
+        return NULL;
+    }
+
+    if(avcodec_parameters_to_context(avCtx, fmtHandle->fmtCtx->streams[fmtHandle->videoIndex]->codecpar) != 0){
+        fprintf(stderr, "Failed to copy codecpar to context\n");
+        return NULL;
+    }
+
+    if(avcodec_open2(avCtx, decoder, NULL) != 0){
+        fprintf(stderr, "avContext failed to init\n");
+        return NULL;
+    }
+    return avCtx;
+}
+
+
 //This function creates different resoultions of video from a single video
 int genMultiResolution(AVHandle *fmtHandle){
+    AVCodecContext *avCtx = createCodecContext(fmtHandle);
+    if(avCtx == NULL){
+        fprintf(stderr, "avCtx failed to init");
+        return 1;
+    }
+
+    AVCodecContext *downScaleAvCtx = createCodecContext(fmtHandle);
+    if(downScaleAvCtx == NULL){
+        fprintf(stderr, "avCtx failed to init");
+        return 2;
+    }
+
+    AVFrame *frame = createFrame(fmtHandle, 1);
+    if(frame == NULL){
+        fprintf(stderr, "frame failed to init\n");
+        return 3;
+    }
+
+    AVFrame *downScaleFrame = createFrame(fmtHandle, 2);
+    if(downScaleFrame == NULL){
+        fprintf(stderr, "downScaleFrame failed to init\n");
+        return 4;
+    }
+
+    int width = fmtHandle->fmtCtx->streams[fmtHandle->videoIndex]->codecpar->width;
+    int height = fmtHandle->fmtCtx->streams[fmtHandle->videoIndex]->codecpar->height;
+    int format = fmtHandle->fmtCtx->streams[fmtHandle->videoIndex]->codecpar->format;
+    struct SwsContext *swsCtx = sws_getContext(width, height, format, width / 2, height / 2, format, SWS_BICUBIC, NULL, NULL, NULL);
+
+    AVPacket *pkt = av_packet_alloc();
+    AVPacket *downScalePkt = av_packet_alloc();
+    while(av_read_frame(fmtHandle->fmtCtx, pkt) == 0){
+        if(pkt->stream_index == fmtHandle->videoIndex){
+            if(avcodec_send_packet(avCtx, pkt) == 0){
+                while(avcodec_receive_frame(avCtx, frame) == 0){
+                    sws_scale(swsCtx, (uint8_t const* const*)frame->data, frame->linesize, 0, frame->height, downScaleFrame->data, downScaleFrame->linesize);
+                    avcodec_send_frame(downScaleAvCtx, downScaleFrame);
+                    avcodec_receive_packet(downScaleAvCtx, downScalePkt);
+                }
+            }
+        }
+        av_packet_unref(pkt);
+    }
+
+    av_packet_unref(pkt);
+    av_frame_free(&frame);
+    av_frame_free(&downScaleFrame);
+    avcodec_free_context(&avCtx);
+
     return 0;
 }
 
@@ -49,5 +143,8 @@ int main(){
         fprintf(stderr, "Error occured during parseVideoFile");
         return 1;
     }
+    genMultiResolution(fmtHandle);
+    avformat_close_input(&(fmtHandle->fmtCtx));
+    free(fmtHandle);
     return 0;
 }
